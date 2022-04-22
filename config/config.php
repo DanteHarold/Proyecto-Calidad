c8149867f8ec9a57e1624ea8115be8e13efcdabc
DanteHarold <novianrelay@gmail.com>
2022-04-22 12:43:29 -0500

    Db Credenciales

--- config/config.php
+++ config/config.php
@@ -1,10 +1,10 @@
 <?php
     define('URL', 'http://localhost/Proyecto-Calidad/');
 
-    define('HOST', 'localhost');
-    define('DB', 'sistema_db');
-    define('USER', 'postgres');
-    define('PASSWORD', "99753");
+    define('HOST', 'ec2-44-199-143-43.compute-1.amazonaws.com');
+    define('DB', 'db9kgmov4l6tt1');
+    define('USER', 'hvjfcbdoylasyn');
+    define('PASSWORD', "1b5c0caf3b32b01d0e3a1fff13cf628095c96ac69538d5fc9e5a021102b47520");
     define('CHARSET', 'utf8mb4');
     define('PORT', '5432');
 ?>
